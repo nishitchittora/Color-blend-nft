9388e9cf64d9d6a2527e0b7e6a1dd0297078a4d6
nishitchittora <nishitchittora2015@gmail.com>
2021-11-14 10:43:26 +0530

    migration updated

--- migrations/2_deploy_migration.js
+++ migrations/2_deploy_migration.js
@@ -1,5 +1,18 @@
 const Color = artifacts.require("Color");
+const ColorAccessControl = artifacts.require("ColorAccessControl");
+const ColorBase = artifacts.require("ColorBase");
+const ColorOwnership = artifacts.require("ColorOwnership");
+const ColorAuction = artifacts.require("ColorAuction");
+const ColorMinting = artifacts.require("ColorMinting");
+const ColorCore = artifacts.require("ColorCore");
+
 
 module.exports = function(deployer) {
-  deployer.deploy(Color);
+    deployer.deploy(Color);
+    deployer.deploy(ColorAccessControl);
+    deployer.deploy(ColorBase);
+    deployer.deploy(ColorOwnership);
+    deployer.deploy(ColorAuction);
+    deployer.deploy(ColorMinting);
+    deployer.deploy(ColorCore);
 };
